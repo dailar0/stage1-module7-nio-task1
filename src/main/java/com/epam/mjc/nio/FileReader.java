41e385e3ffeb0f7c9ef41c76d9baced219b5f700
dailar <dailarse00@gmail.com>
2023-02-01 14:17:35 +0300

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+
+            String nameString = reader.readLine();
+            String ageString = reader.readLine();
+            String emailString = reader.readLine();
+            String phoneString = reader.readLine();
+
+            String name = nameString.split(":")[1].strip();
+            int age = Integer.parseInt(ageString.split(":")[1].strip());
+            String email = emailString.split(":")[1].strip();
+            long phone = Long.parseLong(phoneString.split(":")[1].strip());
+
+            return new Profile(name, age, email, phone);
+        } catch (IOException e) {
+            return new Profile();
+        }
+
     }
 }
